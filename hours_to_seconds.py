9777537c70691766e4f707cc3a71339d8296c4b4
jdam-github <jdam600@gmail.com>
2024-06-26 14:09:31 +0200

    initial commit

---
+++ hours_to_seconds.py
@@ -0,0 +1,10 @@
+# Define a variable named hours and assign it a value of 2
+hours = 2
+
+# Calculate the number of seconds in the given hours
+# There are 3600 seconds in an hour
+seconds = hours * 3600
+
+# Print the result
+print(f"{hours} hour(s) is {seconds} seconds.")
+
