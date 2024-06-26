f2d278e7ec50bb1ce78f0ece2574329ac5317021
jdam-github <jdam600@gmail.com>
2024-06-26 14:57:53 +0200

    initial commit

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,3 +1,3 @@
-age = int(input("how old are you " ))
-future_age= age +27
-print(f"In 2050, you will be {future_age} years old.")
+age= int(input("how old are you"))
+future_age= age + 27
+print(f"in 2050 you will be {future_age} years old")
