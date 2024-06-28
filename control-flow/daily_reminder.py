fd38437dc4ea05702d4e74f5d57d5ce9f88f19a9
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-06-28 17:17:03 +0200

    Delete control-flow/daily_reminder.py

--- control-flow/daily_reminder.py
+++
@@ -1,9 +0,0 @@
-task= str(input("Enter your task:"))
-choice= str(input("Priority (high/medium/low):"))
-bound= str(input("Is it time-bound?"))
-if choice == "high" and bound == "yes":
-    print("'Finish project report' is a high priority task that requires immediate attention today!")
-elif choice == "low" and bound =="no":
-    print("Read a book' is a low priority task. Consider completing it when you have free time")
-else:
-    print("pleas try again")
