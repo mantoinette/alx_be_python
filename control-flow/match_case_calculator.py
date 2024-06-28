3fb6b3c5cb96f7e81aa48a7b065d987fbb57fb82
jdam-github <jdam600@gmail.com>
2024-06-28 13:18:11 +0200

    initial commit

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,28 +1,29 @@
-
+def main():
 # Prompt user for input
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-operation = input("Choose the operation (+, -, *, /): ")
+ num1 = float(input("Enter the first number: "))
+ num2 = float(input("Enter the second number: "))
+ operation = input("Choose the operation (+, -, *, /): ")
 
 # Perform the calculation based on the chosen operation
-match operation:
+ 
+ match operation:
   case "+":
     result = num1 + num2
-    print(f"result is {result}")
+    print(f"the result is {result}")
   case "-":
     result = num1 - num2
-    print(f"result is {result}")
+    print(f" the result is {result}")
   case "*":
     result = num1 * num2
-    print(f"result is {result}")
+    print(f"the result is {result}")
   case "/":
     if num2 == 0:
         print("Division by zero is not allowed")
     else:
         result = num1 / num2
-        print(f"result is {result}")
+        print(f"the result is {result}")
   case _:
     print("Invalid operation. Please choose from '+', '-', '*', '/'.")
-    if __name__ == "__main__":
+if __name__ == "__main__":
      main()
 
