5816a18c14a990288155b264efbe2e031166316e
jdam-github <jdam600@gmail.com>
2024-06-28 16:25:38 +0200

    initial commit

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,16 @@
-user = int(input("Enter the size of the pattern: "))
+# Ask the user to input the size of the pattern
+size = int(input("Enter the size of the pattern: "))
+
+# Initialize the row counter
 i = 1
-while i <= user:
-    print("*" * user, end="")  # Print '*' repeated 'user' times for each row without newline
+
+# Loop through each row
+while i <= size:
+    # Loop through each column in the current row
+    for j in range(size):
+        # Print an asterisk without a newline
+        print("*", end="")
+    # Print a newline character to move to the next row
+    print()
+    # Increment the row counter
     i += 1
