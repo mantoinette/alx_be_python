4dc1d40a9d1b03b296fd06b1f194525a58b94cec
jdam-github <jdam600@gmail.com>
2024-06-28 17:18:58 +0200

    initial commit

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,9 +1,11 @@
-task= str(input("Enter your task:"))
-choice= str(input("Priority (high/medium/low):"))
-bound= str(input("Is it time-bound?"))
-if choice == "high" and bound == "yes":
-    print("'Finish project report' is a high priority task that requires immediate attention today!")
-elif choice == "low" and bound =="no":
-    print("Read a book' is a low priority task. Consider completing it when you have free time")
+Task = str(input("Enter your task: "))
+Priority = str(input("(high/medium/low): "))
+Time_Bound = str(input("Is it time-bound? (yes/no): "))
+if Priority == "high" and Time_Bound == "yes":
+    print(f"'{Task}' is a high priority task that requires immediate attention today!")
+elif Priority == "low" and Time_Bound == "no":
+    print(f"'{Task}' is a low priority task. Consider completing it when you have free time")
 else:
-    print("pleas try again")
+    print("Please try again")
+
+
