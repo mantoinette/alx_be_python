ead9f01988c9f4f2632b0d5b9eae03068c91fd31
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-07-08 16:02:44 +0200

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@
 
 # Define global conversion factors with consistent indentation
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  # Factor to convert Fahrenheit to Celsius
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # Factor to convert Celsius to Fahrenheit
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/ 5  # Factor to convert Celsius to Fahrenheit
 
 def convert_to_celsius(fahrenheit):
     """Converts a temperature from Fahrenheit to Celsius.
